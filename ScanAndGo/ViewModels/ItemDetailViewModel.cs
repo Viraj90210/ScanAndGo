66e5e0f2e04fda2ca8e3a1d917156df12eb26192
Viraj Pratapwant <viraj.pratapwant@jda.com>
2018-11-28 18:28:48 +0530

    Initial check-in of module ScanAndGo

---
+++ ScanAndGo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using ScanAndGo.Models;
+
+namespace ScanAndGo.ViewModels {
+    public class ItemDetailViewModel : BaseViewModel {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null) {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ ScanAndGo/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScanAndGo.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage {
+        public AboutPage() {
+            InitializeComponent();
+        }
+    }
+}
